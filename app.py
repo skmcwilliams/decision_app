498d5f70e7cdd2c42dc30f1d44ac8be604659bb7
SKM <skm@Stevens-MacBook-Air.local>
2022-08-21 16:53:38 -0500

    update

--- app.py
+++ app.py
@@ -91,8 +91,8 @@ app.layout = html.Div(children=[
         html.H1(children='Difference In Difference and Regression Discontinuity Design Models'),
         html.H2(children='DID and RDD models based on project conducted in Quantitative Analytics class as part of MS Data Analytics degree at University of Wisconsin-Whitewater'),
         html.H5(children='Data provided as part of coursework and is to assess performance after a chance. Change being workig from home, performance being measured as Sales'),
-        html.H2(children='Null Hypothesis: Working From Home results in lower productivity.'),
-        html.Markdown(children= 'Please see github for code detail: https://github.com/skmcwilliams/decision_app' ),
+        html.H5(children='Null Hypothesis: Working From Home results in lower productivity.'),
+        html.H5(children= 'Please see github for code detail: https://github.com/skmcwilliams/decision_app' ),
         ]),
     
     
